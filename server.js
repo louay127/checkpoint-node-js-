f0b6f9b6f2b01ea096a88563a7712272ec892081
louay127 <louaykhouni71@gmail.com>
2024-12-03 10:58:39 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello Node !!!!</h1>\n');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+console.log("HELLO WORLD");
+const fs = require('fs');
